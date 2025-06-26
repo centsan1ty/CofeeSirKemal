e95dfdc36b5cd7cdf19b3e5171af9a875658a257
centsan1ty <kakoonaf@gmail.com>
2025-06-26 21:11:27 +0700

    tahap3

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbarNav = document.querySelector(".navbar-nav");
+
+//hamburger menu responsif
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
