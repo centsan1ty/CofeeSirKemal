f1191cbfd72feaad9911ff3e5f300e3e0f3878a1
centsan1ty <kakoonaf@gmail.com>
2025-06-26 23:17:09 +0700

    tahap 9

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
 };
 
 const hamburger = document.querySelector("#hamburger-menu");
+
+// toggle class active search icon
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm .classList.remove("active");
+  }
+
+});
